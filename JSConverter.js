6827c12091ab2989bc047725e8cfd0789190708c
Roman Sokolov <still2.mail@me.com>
2017-01-20 16:46:46 +0300

    remove JSConverter.js

--- JSConverter.js
+++
@@ -1,7 +0,0 @@
-function jsonFromFilters(rules, limit, optimize){
-    try {
-        return SafariContentBlockerConverter.convertArray(rules, limit, optimize);
-    } catch (ex) {
-        console.log('Unexpected error: ' + ex);
-    }
-};
